8e528ba34bbd714f924873994791156c587d31f0
FM-Akib <muntasirakib08@gmail.com>
2024-02-20 01:11:28 +0600

    UI

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,23 @@
+import Marquee from "react-fast-marquee";
 import Header from "./Header";
+import NavBar from "./NavBar";
 
 const Home = () => {
     return (
         <div className="font-popins">
-            <Header/>
+        <Header/>
+
+        <div className="w-[1400px] mx-auto flex">
+            <div className="bg-red-700 w-28 p-4 rounded-md text-white justify-center flex text-center items-center ">
+                Breaking News
+            </div>
+            <Marquee speed={150} className="bg-gray-200">
+            <p className="text-xl">I can be a React component, multiple React components, or just some text.</p>
+            </Marquee>
+        </div>
+
+        <NavBar/>
+
         </div>
     );
 };
