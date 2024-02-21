08c391bea407a5fdd82c95bcc84672de20db8a06
FM-Akib <muntasirakib08@gmail.com>
2024-02-21 19:33:04 +0600

    right side updated

--- src/Shared/RightNav.jsx
+++ src/Shared/RightNav.jsx
@@ -4,10 +4,16 @@ import { FaFacebook } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
+
+import q1 from '../assets/qZone1.png'; 
+import q2 from '../assets/qZone2.png'; 
+import q3 from '../assets/qZone3.png'; 
+import add1 from '../assets/advertise.png'; 
+
 const RightNav = () => {
     
     return (
-        <div className="sticky top-24">
+        <div className="">
             {/* login with */}
             <div className="mb-5">
                 <h2 className="text-2xl mb-2">Login with</h2>
@@ -28,7 +34,17 @@ const RightNav = () => {
             </div>
             </div>
 
-
+            {/* Q-zone */}
+            <div className="mt-8 bg-slate-200 p-1 rounded-md">
+            <h1 className="my-2 ml-2 font-bold">Q Zone</h1>
+            <img src={q1} alt=""/>
+            <img src={q2} alt=""/>
+            <img src={q3} alt=""/>
+            </div>
+ 
+            <div className="mt-8">
+                <img src={add1}/>
+            </div>
         </div>
     );
 };
