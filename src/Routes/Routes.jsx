0c29b29c36fd72be39d19f3f664de5b5df46fbf7
FM-Akib <muntasirakib08@gmail.com>
2024-02-21 21:41:26 +0600

    login and signup updated

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import Signup from "../Pages/Signup";
 
 const router = createBrowserRouter([
     {
@@ -8,8 +10,16 @@ const router = createBrowserRouter([
       element: <Layout></Layout>,
       children: [
         {
-            path: "",
+            path: "/",
             element: <Home></Home>
+        },
+        {
+          path: "login",
+          element: <Login></Login>
+        },
+        {
+          path: "signup",
+          element: <Signup></Signup>
         }
     ]
     },
