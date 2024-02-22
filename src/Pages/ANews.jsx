b95f6948c6c1f5f3d9d6779fe42317b1019e1816
FM-Akib <muntasirakib08@gmail.com>
2024-02-22 14:37:39 +0600

    news details are done

--- src/Pages/ANews.jsx
+++ src/Pages/ANews.jsx
@@ -6,39 +6,40 @@ import RightNav from "../Shared/RightNav";
 
 const ANews = () => {
     const {news} = useContext(NewsContext);
-     console.log(news)
+    //  console.log(news)
     const {id} = useParams();
     const newsDetails = news.filter(obj => obj._id === id);
-    console.log(newsDetails )
-    console.log(typeof(id) )
+    // console.log(newsDetails )
+    // console.log(typeof(id) )
+    const {title,details,image_url,total_view,author} = newsDetails[0];
+    // console.log(title)
     
     return (
         <div className="font-popin w-[1400px]  mx-auto">
            <Header/>
             
-            <div className="grid grid-cols-4 ">
+            <div className="grid grid-cols-4 mt-8">
 
-                <div className="col-span-3 bg-orange-400 p-8">
-
+                <div className="col-span-3  p-8">
                 <div className="max-w-5xl p-4 rounded-lg shadow-md bg-gray-50 text-gray-800">
 	<div className="flex justify-between pb-4 border-bottom">
 		<div className="flex items-center">
-			<a rel="noopener noreferrer" href="#" className="mb-0 capitalize text-gray-800">Photography</a>
+			<a rel="noopener noreferrer" href="#" className="mb-0 capitalize text-gray-800">Total View - {total_view}</a>
 		</div>
 		<a rel="noopener noreferrer" href="#">See All</a>
 	</div>
 	<div className="space-y-4">
 		<div className="space-y-2">
-			<img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-72 bg-gray-500" />
-			<div className="flex items-center text-xs">
-				<span>6 min ago</span>
+			<img src={image_url} alt="" className="block object-cover object-center w-full rounded-md  bg-gray-500" />
+			<div className="flex items-center text-xl font-semibold">
+				<span>{author.name}</span>
 			</div>
 		</div>
-		<div className="space-y-2">
+		<div className="space-y-3">
 			<a rel="noopener noreferrer" href="#" className="block">
-				<h3 className="text-xl font-semibold text-amber-600">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
+				<h3 className="text-2xl font-bold text-amber-600">{title}</h3>
 			</a>
-			<p className="leadi text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
+			<p className=" text-gray-800 text-xl leading-8 p-2">{details}</p>
 		</div>
 	</div>
                 </div>
