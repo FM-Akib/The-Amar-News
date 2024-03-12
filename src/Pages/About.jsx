c4a07115004b42b5e7944fddd2817ef19481e14b
FM-Akib <muntasirakib08@gmail.com>
2024-03-13 01:20:07 +0600

    slider added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,39 +1,19 @@
-
-
-import Header from "./Header"
-import NavBar from "./NavBar"
-import { useState } from "react";
-
-
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import SwipeCard from "../components/SwipeCard";
+import Header from "./Header";
+import NavBar from "./NavBar";
 
 const About = () => {
-    const [value, setValue] = useState(2);
   return (
-    <div className="font-popin w-[1400px]  mx-auto">
-    <Header/>
-    <NavBar/>
-    
-   {/* review */}
-    <Box
-      sx={{
-        '& > legend': { mt: 2 },
-      }}
-    >
-      <Typography component="legend">Controlled</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      /> </Box>
+    <div>
+      <Header/>
+      <NavBar/>
+      
+      <h1>Feature</h1>
 
+     <SwipeCard/>
 
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
